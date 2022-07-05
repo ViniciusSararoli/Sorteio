f3e0c6ec989fc5ed38aa0f8463d73608668ed1d7
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2022-07-05 20:35:07 -0300

    Add funcionalidade para sorteio

--- Sorteio/app/src/main/java/com/example/sorteio/MainActivity.java
+++ Sorteio/app/src/main/java/com/example/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selcionarBotao(View v) {
+        int numero = new Random().nextInt(11);
+        TextView txt = findViewById(R.id.txtResultado);
+        txt.setText("O número sorteado foi: " + numero);
     }
 }
